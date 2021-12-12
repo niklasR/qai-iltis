1d4f518f6623dea4ffbab46d5ea060bc880e7b83
Nik Rahmel <nik@rahmel.eu>
2021-12-12 17:50:59 +0000

    Task: Use real image data in ImageChroma component

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 TODO
 
 - move message to seen after x loops (add NumberOfLoops to config)
+- fix bug: image distortion
 - New UI: fullscren images
-- New UI: chroma images (same as fullscreen but w chroma background, smaller and shifted to left of frame)
 - add a 'SHOW TICKER' button that fades it up + starts it / hides with fade out transition
 - (alex) move some vars to extra ts file / json / yml
 - (alex) neaten up message cards
@@ -12,5 +12,6 @@ TODO
 
 
 DONE
+- New UI: chroma images (same as fullscreen but w chroma background, smaller and shifted to left of frame)
 - add 'ignore' button to all message panes (and make functional)
 - DONT SHOW MESSAGES WITH IMAGE IN ThE TICKER!!!
--- src/client/Components/ImageChroma.tsx
+++ src/client/Components/ImageChroma.tsx
@@ -7,56 +7,65 @@ import { AppData, MessageState } from '../../model';
 export function ImageChroma({ appData, backgroundColor }: { appData: AppData, backgroundColor: string }): React.ReactElement {
   const theme = useTheme();
 
+  const message = appData.messages.find((message) => {
+    const isShowing = message.state === MessageState.SHOWING;
+    const hasAttachment = message.attachment;
+    return isShowing && hasAttachment;
+  });
+
+  const messageBox = message && (<Box sx={{
+    position: 'absolute',
+    height: '864px',
+    width: '768px',
+    left: '880px',
+    top: '108px',
+    backgroundColor: theme.palette.primary.main
+  }}>
+    {/* Name Box */}
+    <Box sx={{
+      position: 'absolute',
+      height: '100px',
+      width: '100%',
+      top: '20px',
+      paddingLeft: '10px',
+      paddingRight: '10px',
+      backgroundColor: theme.palette.primary.dark
+    }}>
+      <Typography fontSize={60} paragraph color='common.white'>
+        {message.from}
+      </Typography>
+    </Box>
+    {/* Image 'Box */}
+    <Box sx={{
+      position: 'absolute',
+      height: '500px',
+      width: '100%',
+      top: '160px',
+      backgroundColor: '#666'
+    }
+    }>
+      <img height="100%" width="100%" src={`data:${message.attachment.mimetype};base64,${message.attachment.data}`} />
+    </Box>
+    {/* Caption Box */}
+    <Box sx={{
+      position: 'absolute',
+      height: '140px',
+      width: '100%',
+      bottom: '20px',
+      paddingLeft: '10px',
+      paddingRight: '10px',
+      backgroundColor: theme.palette.primary.dark
+    }
+    }>
+      <Typography fontSize={40} paragraph color='common.white'>
+        {message.text}
+      </Typography>
+    </Box>
+  </Box>);
+
   return (
     <Box sx={{ display: 'fixed', position: 'relative', height: '1080px', width: '1920px', backgroundColor, overflow: 'hidden' }}>
-      <Box sx={{
-        position: 'absolute',
-        height: '864px',
-        width: '768px',
-        left: '880px',
-        top: '108px',
-        backgroundColor: theme.palette.primary.main
-      }}>
-        {/* Name Box */}
-        <Box sx={{
-          position: 'absolute',
-          height: '100px',
-          width: '100%',
-          top: '20px',
-          paddingLeft: '10px',
-          paddingRight: '10px',
-          backgroundColor: theme.palette.primary.dark
-        }}>
-          <Typography fontSize={60} paragraph color='common.white'>
-            Thomas Sizemorelong
-          </Typography>
-        </Box>
-        {/* Image 'Box */}
-        <Box sx={{
-          position: 'absolute',
-          height: '500px',
-          width: '100%',
-          top: '160px',
-          backgroundColor: '#666'
-        }
-        }>
-        </Box>
-        {/* Caption Box */}
-        <Box sx={{
-          position: 'absolute',
-          height: '140px',
-          width: '100%',
-          bottom: '20px',
-          paddingLeft: '10px',
-          paddingRight: '10px',
-          backgroundColor: theme.palette.primary.dark
-        }
-        }>
-          <Typography fontSize={40} paragraph color='common.white'>
-            Thomas had something rather witty to say about this image.
-          </Typography>
-        </Box>
-      </Box>
+      {messageBox}
     </Box >
   );
 }
