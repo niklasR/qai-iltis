fa2e372898b3a718cef6c30ee313e04df4cb45ca
Nik Rahmel <nik@rahmel.eu>
2021-12-12 22:52:51 +0000

    Task: Chaneg ticker animation to Slide

--- src/client/Components/Ticker.tsx
+++ src/client/Components/Ticker.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import Fade from '@mui/material/Fade';
+import Slide from '@mui/material/Slide';
 import ReactTicker from 'react-ticker';
 import PageVisibility from 'react-page-visibility';
 import { AppData, MessageState } from '../../model';
@@ -18,37 +18,37 @@ export function Ticker({ appData, backgroundColor }: { appData: AppData, backgro
 
   return (
     <Box sx={{ display: 'fixed', position: 'relative', height: '1080px', width: '1920px', backgroundColor, overflow: 'hidden' }}>
-      <Fade in={appData.elements.ticker.show}>
-        <Box sx={{
-          position: 'absolute',
-          height: '60px',
-          width: '100%',
-          bottom: '20px',
-          backgroundColor: theme.palette.primary.main
-        }}>
-          <PageVisibility onChange={handleVisibilityChange}>
-            {pageIsVisible && (
-              <ReactTicker offset={0} speed={25}>
-                {({ index }) => {
-                  const activeMessages = appData.messages.filter((message) => {
-                    const isShowing = message.state === MessageState.SHOWING;
-                    const hasNoAttachement = !message.attachment;
-                    return isShowing && hasNoAttachement;
-                  });
-                  const nextMessage = activeMessages[index % activeMessages.length];
-                  return nextMessage ? (
-                    <Typography paragraph sx={{ fontSize: 40, whiteSpace: 'nowrap', paddingRight: 20 }}>
-                      {nextMessage.from}: {nextMessage.text}
-                    </Typography>
-                  ) : (
-                    <p style={{ visibility: "hidden" }}>Placeholder</p>
-                  );
-                }}
-              </ReactTicker>
-            )}
-          </PageVisibility>
-        </Box>
-      </Fade>
-    </Box>
+      <Slide direction="up" in={appData.elements.ticker.show} timeout={500}>
+      <Box sx={{
+        position: 'absolute',
+        height: '100px',
+        width: '100%',
+        bottom: '30px',
+        backgroundColor: theme.palette.primary.main
+      }}>
+        <PageVisibility onChange={handleVisibilityChange}>
+          {pageIsVisible && (
+            <ReactTicker offset={0} speed={25}>
+              {({ index }) => {
+                const activeMessages = appData.messages.filter((message) => {
+                  const isShowing = message.state === MessageState.SHOWING;
+                  const hasNoAttachement = !message.attachment;
+                  return isShowing && hasNoAttachement;
+                });
+                const nextMessage = activeMessages[index % activeMessages.length];
+                return nextMessage ? (
+                  <Typography paragraph sx={{ fontSize: 70, whiteSpace: 'nowrap', paddingRight: 20 }}>
+                    {nextMessage.from}: {nextMessage.text}
+                  </Typography>
+                ) : (
+                  <p style={{ visibility: "hidden" }}>Placeholder</p>
+                );
+              }}
+            </ReactTicker>
+          )}
+        </PageVisibility>
+      </Box>
+    </Slide>
+    </Box >
   );
 }
